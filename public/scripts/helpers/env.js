7432e7f78c372b12f8690f609a76333f6595356e
Javier Centurión <javier.centurion@auth0.com>
2015-12-04 15:56:25 -0300

    Update styles version

--- public/scripts/helpers/env.js
+++ public/scripts/helpers/env.js
@@ -2,10 +2,10 @@
   'use strict';
 
   context.env = {
-    // clientId:    'sOZFZyH5MXSRH3uqmjjHf5eT7GYWAjIx',
-    // authUrl:     'https://auth0.dev.auth0.com/i/oauth2/authorize',
-    clientId:    'N3PAwyqXomhNu6IWivtsa3drBfFjmWJL',
-    authUrl:     'https://auth0.auth0.com/i/oauth2/authorize',
+    clientId:    'sOZFZyH5MXSRH3uqmjjHf5eT7GYWAjIx',
+    authUrl:     'https://auth0.dev.auth0.com/i/oauth2/authorize',
+    // clientId:    'N3PAwyqXomhNu6IWivtsa3drBfFjmWJL',
+    // authUrl:     'https://auth0.auth0.com/i/oauth2/authorize',
     callbackUrl: 'http://localhost:3000/callback.html',
     // TODO: Rollback when API is fixed
     scopes:      'read:connections create:connections update:connections delete:connections read:clients read:users', //openid profile
