ffdd641a470c8b7e46f1d0d1a32735a21619cbfc
jcenturion <javier.centurion@auth0.com>
2016-09-20 14:40:47 -0300

    Add support for RTA

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ var Webtask  = require('webtask-tools');
 var app      = express();
 var template = require('./views/index.jade');
 
-app.use(auth0({
-  clientName: 'Custom Social Connections',
-  scopes:     'read:connections create:connections update:connections delete:connections read:clients',
-  audience:   function (req) {
-    return 'https://'+req.webtaskContext.data.AUTH0_DOMAIN+'/api/v2/';
-  }
-}));
+app.use(function (req, res, next) {
+  auth0({
+    clientName: 'Custom Social Connections',
+    scopes:     'read:connections create:connections update:connections delete:connections read:clients',
+    audience:   function (req) {
+      return 'https://'+req.webtaskContext.data.AUTH0_DOMAIN+'/api/v2/';
+    },
+    rootTenantAuthority: req.webtaskContext.data.AUTH0_RTA
+  })(req, res, next);
+});
 
 app.get('/', function (req, res) {
   res.header("Content-Type", 'text/html');
