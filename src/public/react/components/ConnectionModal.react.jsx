42e0cd47409d081cdf5757501d4260bfcb6f8e04
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2017-05-30 13:23:27 -0300

    replace hardcoded manage url
    fixes #16

--- src/public/react/components/ConnectionModal.react.jsx
+++ src/public/react/components/ConnectionModal.react.jsx
@@ -88,7 +88,7 @@ var ConnectionModal = React.createClass({
       '&scope=openid%20profile',
       '&client_id=' + window.env.masterClientId,
       '&connection=' + this.state.connection.name,
-      '&redirect_uri=https://manage.auth0.com/tester/callback?connection=' + this.state.connection.name
+      '&redirect_uri=' + window.env.manageUrl + '/tester/callback?connection=' + this.state.connection.name
     ].join('');
   },
 
