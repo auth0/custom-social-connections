cac3263b98705dd12ff266b1e8c07daaeb4246a1
José Padilla <jpadilla@webapplicate.com>
2020-07-16 14:48:16 -0400

    Make sure we verify issuer (#48)
    * Make sure we verify issuer
    
    * Bump up version

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express  = require('express');
 var auth0    = require('auth0-oauth2-express');
+var jwt      = require('jsonwebtoken')
 var Webtask  = require('webtask-tools');
 var app      = express();
 var template = require('./views/index.jade');
@@ -7,14 +8,34 @@ var template = require('./views/index.jade');
 app.use(express.static(__dirname + '/public'));
 
 app.use(function (req, res, next) {
-  auth0({
-    clientName: 'Custom Social Connections',
-    scopes:     'read:connections create:connections update:connections delete:connections read:clients',
-    audience:   function (req) {
-      return 'https://'+req.webtaskContext.data.AUTH0_DOMAIN+'/api/v2/';
+  var middleware = auth0({
+    clientName: "Custom Social Connections",
+    scopes:
+      "read:connections create:connections update:connections delete:connections read:clients",
+    audience: function (req) {
+      return "https://" + req.webtaskContext.data.AUTH0_DOMAIN + "/api/v2/";
     },
-    rootTenantAuthority: req.webtaskContext.data.AUTH0_RTA
-  })(req, res, next);
+    rootTenantAuthority: req.webtaskContext.data.AUTH0_RTA,
+    authenticatedCallback: function (req, res, accessToken, next) {
+      /**
+       * Note: We're normalizing the issuer because the access token `iss`
+       * ends in a slash whereas the `AUTH0_RTA` secret does not.
+       */
+      var rta = req.webtaskContext.data.AUTH0_RTA || "https://auth0.auth0.com";
+      var expectedIssuer = rta.endsWith("/") ? rta : rta + "/";
+      var dtoken = jwt.decode(accessToken) || {};
+
+      if (dtoken.iss !== expectedIssuer) {
+        return res.json({
+          message: "jwt issuer invalid. expected: " + expectedIssuer
+        });
+      }
+
+      return next();
+    },
+  });
+
+  return middleware(req, res, next);
 });
 
 app.get('/', function (req, res) {
