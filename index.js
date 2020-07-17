2bf46c24c798c32aaaac302ba4490b9d2e2c5d5d
Chris Geihsler <chris.geihsler@auth0.com>
2020-07-17 09:28:05 -0400

    Backport RTA issuer check to 1.1.x

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 var express  = require('express');
 var auth0    = require('auth0-oauth2-express');
+var jwt      = require('jsonwebtoken');
 var Webtask  = require('webtask-tools');
 var app      = express();
 var template = require('./views/index.jade');
 
 app.use(function (req, res, next) {
+  const rta =  req.webtaskContext.data.AUTH0_RTA || "https://auth0.auth0.com";
+
   auth0({
     clientName: 'Custom Social Connections',
     scopes:     'read:connections create:connections update:connections delete:connections read:clients',
     audience:   function (req) {
       return 'https://'+req.webtaskContext.data.AUTH0_DOMAIN+'/api/v2/';
     },
-    rootTenantAuthority: req.webtaskContext.data.AUTH0_RTA
+    rootTenantAuthority: rta,
+    authenticatedCallback: function (req, res, accessToken, next) {
+      /**
+       * Note: We're normalizing the issuer because the access token `iss`
+       * ends in a slash whereas the `AUTH0_RTA` secret does not.
+       */
+      var rta = req.webtaskContext.data.AUTH0_RTA || "https://auth0.auth0.com";
+      var expectedIssuer = rta.endsWith("/") ? rta : rta + "/";
+      var dtoken = jwt.decode(accessToken) || {};
+
+      if (dtoken.iss !== expectedIssuer) {
+        return res.json({
+          message: "jwt issuer invalid. expected: " + expectedIssuer
+        });
+      }
+
+      return next();
+    },
   })(req, res, next);
 });
 
